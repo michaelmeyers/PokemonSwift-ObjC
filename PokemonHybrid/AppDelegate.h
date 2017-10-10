50b89555b419c5791c4739541081dc85ce2cfb13
michaelmeyers <31745265+michaelmeyers@users.noreply.github.com>
2017-10-10 10:05:43 -0600

    Initial Commit

---
+++ PokemonHybrid/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PokemonHybrid
+//
+//  Created by Michael Meyers on 10/10/17.
+//  Copyright © 2017 Michael Meyers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PokemonHybrid/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PokemonHybrid
+//
+//  Created by Michael Meyers on 10/10/17.
+//  Copyright © 2017 Michael Meyers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
